e76ec880f681ecda6277ccf7c3ed8cc2e36d8ef7
lukasx <llukasxx@gmail.com>
2015-04-25 12:29:56 -0700

    Finish user edit, update, index, add admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:                  "lukasx",
+             email:                 "llukasxx@gmail.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin:                 true)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@gmail.com"
+  User.create!(name:                  name,
+               email:                 email,
+               password:              "password",
+               password_confirmation: "password")
+end
